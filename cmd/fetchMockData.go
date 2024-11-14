aee90322598a570c96b0e87e0f8ed8f33bf1ca36
livanjimenez <jimenez.livan14@gmail.com>
2024-11-13 23:20:21 -0500

    p

---
+++ cmd/fetchMockData.go
@@ -0,0 +1,25 @@
+package cmd
+
+import "github.com/livanjimenez/env-monitor/data"
+
+func FetchAirQualityData() []data.AirQualityData {
+	CO := data.GenerateMockData().CO
+	CO2 := data.GenerateMockData().CO2
+	Humidity := data.GenerateMockData().Humidity
+	PM10 := data.GenerateMockData().PM10
+	PM25 := data.GenerateMockData().PM25
+	O3 := data.GenerateMockData().O3
+	Temperature := data.GenerateMockData().Temperature
+
+	return []data.AirQualityData{
+		{
+			CO:          CO,
+			CO2:         CO2,
+			Humidity:    Humidity,
+			PM10:        PM10,
+			PM25:        PM25,
+			O3:          O3,
+			Temperature: Temperature,
+		},
+	}
+}
